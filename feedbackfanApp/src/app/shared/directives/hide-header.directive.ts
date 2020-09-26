fea1c923a13a2c7c003a970563df63a6c4bf0ce7
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-09-26 12:55:39 -0400

    spash screen y modificaciones visuales

--- feedbackfanApp/src/app/shared/directives/hide-header.directive.ts
+++ feedbackfanApp/src/app/shared/directives/hide-header.directive.ts
@@ -19,7 +19,7 @@ export class HideHeaderDirective implements OnInit {
   ngOnInit(): void {
       this.header = this.header.el;
       this.domCtrl.write(() => {
-          this.renderer.setStyle(this.header, 'transition', 'margin-top 200ms');
+          this.renderer.setStyle(this.header, 'transition', 'margin-top 50ms');
       });
   }
 
